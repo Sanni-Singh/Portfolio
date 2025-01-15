e175d1805186ea6db03614d08866e4541b3e68e2
Sanni-Singh <aryansingh7371@gmail.com>
2025-01-15 16:42:50 +0530

    chnages on footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -32,14 +32,14 @@ const FooterComponent = ()=>{
             backgroundColor: 'transparent',
         }} className="relative flex  items-end h-[20rem] bg-transparent place-content-center overflow-hidden px-4  text-grey-200">
 
-            <div className="text-white  w-screen bg-transparent flex justify-between items-center px-10 py-5">
+            <div className="text-white  w-screen bg-transparent flex justify-between items-center sm:px-10 py-5">
                 <div>
-                    <h3 className="text-xl flex gap-2">Bihar,<span>India</span></h3>
+                    <h3 className="sm:text-xl text-md flex gap-2">Bihar,<span>India</span></h3>
                     <p>{tarik}</p>
                 </div>
-                <p className="flex justify-center items-center gap-3 text-xl">Made With 💓 by <span className="text-3xl text-bold">Sanniii</span></p>
+                <p className="flex justify-center text-center items-center gap-3 sm:text-xl text-md">Made With 💓 by Sanni</p>
                 <a href="#Home">
-                <motion.button style={{border,borderShadow}} className=" bg-gray-950/10 text-gray-50 transition-colours p-2 rounded-lg cursor-pointer hover:bg-gray-950/50">Back To Top</motion.button>
+                <motion.button style={{border,borderShadow}} className=" bg-gray-950/10 text-gray-50 transition-colours p-2 rounded-lg cursor-pointer hover:bg-gray-950/50 sm:text-xl text-[10px]">Back To Top</motion.button>
                 </a>
             </div>
 
